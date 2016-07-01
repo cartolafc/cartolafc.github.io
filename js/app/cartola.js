925172ea397800967b67e23c57279dd8e1cf9243
Luis Carlos Poletto <lcpoletto@hotmail.com>
2016-06-30 23:36:50 -0400

    Communicating to globo api.

--- js/app/cartola.js
+++ js/app/cartola.js
@@ -1,3 +1,20 @@
 'use strict';
 
 var cartolaApp = angular.module('cartolaApp', []);
+
+cartolaApp.factory('Cartola', ['$http', function($http) {
+  var baseUrl = 'https://api.cartolafc.globo.com/';
+  return {
+    status: function () {
+      var promise = $http(baseUrl + 'mercado/status').then(function(response) {
+        return response.data;
+      });
+
+      return promise;
+    }
+  }
+}]);
+
+cartolaApp.controller('StatusCtrl', ['Cartola', '$scope', function(Cartola, $scope) {
+  $scope.status = Cartola.status();
+}]);
