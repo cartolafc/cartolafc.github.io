5ac9634548743c9c89abb0d631f8c01326dcc07a
Luis Carlos Poletto <luis.poletto@dealertrack.com>
2016-07-06 14:01:03 -0400

    Trying to use cross domain calls.

--- js/app/cartola.js
+++ js/app/cartola.js
@@ -2,11 +2,16 @@
 
 var cartolaApp = angular.module('cartolaApp', []);
 
+cartolaApp.config(function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
 cartolaApp.factory('Cartola', ['$http', function($http) {
   var baseUrl = 'https://api.cartolafc.globo.com/';
   return {
     status: function () {
-      var promise = $http.jsonp(baseUrl + 'mercado/status?callback=JSON_CALLBACK').then(
+      var promise = $http.get(baseUrl + 'mercado/status').then(
           function(response) {
             return response.data;
           },
